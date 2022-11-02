5211a50d9683bbd15f3888d87939c7475a5db65f
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2022-11-02 13:45:00 +0100

    Update 07_weight_converter.py

--- 07_weight_converter.py
+++ 07_weight_converter.py
@@ -2,7 +2,7 @@
 
 # Python 3.9.5
 
-# 07_weight_converter.py
+# weight_converter.py
 
 # Purpose: Convert metric to imperial weight and vice versa
 
@@ -15,9 +15,27 @@ class Weight:
 
     def convert_lbs_to_kg(self):
         return self.weight / 2.2046226
+    
+    def convert_lbs_to_oz(self):
+        return self.weight * 16
 
-weight = 100
+    def convert_oz_to_lbs(self):
+        return self.weight / 16
+
+    def convert_oz_to_gramm(self):
+        return self.weight * 28.3495
+
+    def convert_gramm_to_oz(self):
+        return self.weight / 28.3495
+
+weight = 1
 oWeight = Weight(weight)
 
 oWeight.convert_kg_to_lbs() # Convert kg to lbs
 oWeight.convert_lbs_to_kg() # Convert lbs to kg
+
+oWeight.convert_lbs_to_oz() # Convert lbs to oz
+oWeight.convert_oz_to_lbs() # Convert oz to lbs
+
+oWeight.convert_oz_to_gramm() # Convert oz to gramm
+oWeight.convert_gramm_to_oz() # Convert gramm to oz
